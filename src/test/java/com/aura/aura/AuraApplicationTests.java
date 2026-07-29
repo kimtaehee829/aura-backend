48ed5cd6b46490ffb9e79e977a9cbb5200357cf9
kimtaehee829 <kimhanse000@gmail.com>
2026-07-29 19:17:19 +0900

    chore: Spring Boot 프로젝트 초기화 및 .gitignore 보안 설정

---
+++ src/test/java/com/aura/aura/AuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aura.aura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
